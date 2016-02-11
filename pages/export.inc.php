a45139edcf5e069e5b6259d63340f8d88ddfd1b0
gupi <gunter.pietzsch@prestosoft.de>
2016-02-11 11:04:38 +0100

    Create export.inc.php

---
+++ pages/export.inc.php
@@ -0,0 +1,7 @@
+<?php
+echo "<div id='rex-title'>";
+echo "<div class='rex-title-row'>";
+echo "<h1>Export</h1>";
+echo "</div>";
+echo '<div class="rex-title-row rex-title-row-sub rex-title-row-empty"><p>&nbsp;</p></div>';
+echo '</div>';
